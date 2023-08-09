9289dfedbfb2dbc33deb0d5c896c4a52939b1a56
Guru Mahan <iosdeve2022@gmail.com>
2023-08-09 15:39:26 +0530

    Initial Commit

---
+++ AssicibilityFrameWork/AssicibilityFrameWork.h
@@ -0,0 +1,18 @@
+//
+//  AssicibilityFrameWork.h
+//  AssicibilityFrameWork
+//
+//  Created by Guru Mahan on 09/08/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AssicibilityFrameWork.
+FOUNDATION_EXPORT double AssicibilityFrameWorkVersionNumber;
+
+//! Project version string for AssicibilityFrameWork.
+FOUNDATION_EXPORT const unsigned char AssicibilityFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AssicibilityFrameWork/PublicHeader.h>
+
+
---
+++ AssicibilityFrameWork/ValidatorFile.swift
@@ -0,0 +1,12 @@
+//
+//  ValidatorFile.swift
+//  AssicibilityFrameWork
+//
+//  Created by Guru Mahan on 09/08/23.
+//
+
+import Foundation
+
+public class ValidatorFile {
+    public var isSelected: Bool = true
+}
